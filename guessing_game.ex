f741aaf38962649937eef163f3ecedf9ad41eb3a
cheropChirchir7963 <cheropmarlene@gmail.com>
2021-12-08 11:22:25 +0300

    added fn to guess a smaller number

--- guessing_game.ex
+++ guessing_game.ex
@@ -1,5 +1,9 @@
 defmodule GuessingGame do
 
+  def guess(low, high) do
+    IO.puts "The answer has to be #{mid(low, high)} \n"
+  end
+
   def mid(low, high) do
     div(low + high , 2)
   end
@@ -7,6 +11,11 @@ defmodule GuessingGame do
   def bigger(low, high) do
     new_low = min(high, mid(low,high) + 1)
     guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max(low, mid(low,high) - 1)
+    guess(low, new_high)
   end
 
 end
